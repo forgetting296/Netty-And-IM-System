b5c0343d49d22cf98fab66eb380cc49ce49fa867
zipingliu <1131664758@qq.com>
2020-03-14 15:26:12 +0800

    去掉报错的依赖 成功下载proto插件 新增ByteBuf测试代码

--- src/main/java/com/shusaku/study/bytebuf/ByteBufTypeTest.java
+++ src/main/java/com/shusaku/study/bytebuf/ByteBufTypeTest.java
@@ -2,7 +2,12 @@ package com.shusaku.study.bytebuf;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.CompositeByteBuf;
+import io.netty.buffer.Unpooled;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 
 /**
@@ -12,6 +17,8 @@ import java.nio.charset.Charset;
  * @create: 2020-03-13 15:12
  */
 public class ByteBufTypeTest {
+
+    private final Logger logger = LoggerFactory.getLogger(ByteBufTypeTest.class);
 
     final static Charset UTF_8 = Charset.forName("utf-8");
 
@@ -33,6 +40,88 @@ public class ByteBufTypeTest {
             System.out.println("no has array");
         }
         buffer.release();
+    }
+
+    public void testDirectBuffer() {
+        Charset charset = Charset.forName("UTF-8");
+        ByteBuf directBuffer = ByteBufAllocator.DEFAULT.directBuffer();
+        directBuffer.writeBytes("异乡人　独孤客".getBytes(charset));
+        if(!directBuffer.hasArray()) {
+
+            int readableBytes = directBuffer.readableBytes();
+            byte[] bytes = new byte[readableBytes];
+            directBuffer.readBytes(bytes);
+            logger.info(new String(bytes,charset));
+        }
+
+        directBuffer.release();
+    }
+
+    public void testHeapBuffer() {
+        Charset charset = Charset.forName("UTF-8");
+        ByteBuf heapBuf = ByteBufAllocator.DEFAULT.heapBuffer();
+        heapBuf.writeBytes("异乡人　独孤客".getBytes(charset));
+        if(heapBuf.hasArray()) {
+            int length = heapBuf.readableBytes();
+            byte[] array = heapBuf.array();
+            int offset = heapBuf.readerIndex() + heapBuf.arrayOffset();
+            logger.info(new String(array,offset,length,charset));
+        }
+        heapBuf.release();
+    }
+
+    //我曹为什么最后一个字会乱码
+    public void compositeBufTest() {
+
+        Charset charset = Charset.forName("UTF-8");
+
+        CompositeByteBuf compositeByteBuf = ByteBufAllocator.DEFAULT.compositeBuffer();
+
+        ByteBuf bufHeader = Unpooled.copiedBuffer("叹希奇", charset);
+        ByteBuf bufBody = Unpooled.copiedBuffer("封剑主", charset);
+
+        compositeByteBuf.addComponents(bufHeader,bufBody);
+        sendMessage(compositeByteBuf);
+
+        bufHeader.retain();
+        compositeByteBuf.release();
+
+        compositeByteBuf = ByteBufAllocator.DEFAULT.compositeBuffer();
+        ByteBuf bufBody2 = Unpooled.copiedBuffer("意轩邈", charset);
+        compositeByteBuf.addComponents(bufHeader,bufBody2);
+        sendMessage(compositeByteBuf);
+
+        compositeByteBuf.release();
+    }
+
+    public void sendMessage(CompositeByteBuf compositeByteBuf) {
+        //compositeByteBuf.
+        for(ByteBuf b : compositeByteBuf.decompose(0,compositeByteBuf.maxNumComponents())) {
+            int length = b.readableBytes();
+            byte[] bytes = new byte[length];
+            b.getBytes(b.readerIndex(),bytes);
+            System.out.println(new String(bytes,Charset.forName("UTF-8")));
+            System.out.println();
+        }
+    }
+
+    public void compositeBufTest2() {
+
+        CompositeByteBuf compositeByteBuf = Unpooled.compositeBuffer(3);
+
+        compositeByteBuf.addComponent(Unpooled.wrappedBuffer(new byte[]{1,2,3}));
+        compositeByteBuf.addComponent(Unpooled.wrappedBuffer(new byte[]{4}));
+        compositeByteBuf.addComponent(Unpooled.wrappedBuffer(new byte[]{5,6}));
+
+        //合并成一个缓冲区  参数是开始索引和取合并后的字节数组的长度
+        ByteBuffer buffer = compositeByteBuf.nioBuffer(0, 4);
+
+        byte[] array = buffer.array();
+
+        for (byte b : array) {
+            System.out.print(b);
+        }
+        compositeByteBuf.release();
     }
 
 }
--- src/main/java/com/shusaku/study/protocol/ProtobufDemo.java
+++ src/main/java/com/shusaku/study/protocol/ProtobufDemo.java
@@ -1,5 +1,6 @@
 package com.shusaku.study.protocol;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
@@ -34,6 +35,53 @@ public class ProtobufDemo {
 
         System.out.println("id: = " + msg.getId());
         System.out.println("content: = " + msg.getContent());
+    }
+
+    public void serndDesr() throws IOException {
+
+        Msg.Builder builder = Msg.newBuilder();
+
+        builder.setContent("征尘幂　孤峰披");
+        builder.setId(1);//Msg.getDefaultInstance();
+
+        Msg build = builder.build();
+        byte[] data = build.toByteArray();
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        outputStream.write(data);
+
+        data = outputStream.toByteArray();
+        Msg msg = Msg.parseFrom(data);
+        System.out.println("id: = " + msg.getId());
+        System.out.println("content: = " + msg.getContent());
+    }
+
+    public void serndDesr2() throws IOException {
+
+        Msg msg = buildMsg();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        msg.writeTo(outputStream);
+
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+
+        Msg message = Msg.parseFrom(inputStream);
+
+        System.out.println("id: " + message.getId());
+        System.out.println("content: " + message.getContent());
+
+    }
+
+    public void serAndDesr3() throws IOException {
+        //第三种序列化与反序列化方式　　可以解决半包、粘包问题
+        Msg msg = buildMsg();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        msg.writeDelimitedTo(outputStream);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+        Msg message = Msg.parseDelimitedFrom(inputStream);
+
+        System.out.println("id: " + message.getId());
+        System.out.println("content: " + message.getContent());
     }
 
 }
